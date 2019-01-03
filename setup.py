fa9febd871bb528de19e927e15283fa682b1b6cf
Joe Jarvis <joe.jarvis109@gmail.com>
2019-01-03 12:52:03 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="nativemessaging",
-      version="1.0.0",
+      version="1.0.1",
       description="A package with basic native messaging apis for webextensions",
       long_description=long_description,
       long_description_content_type="text/markdown",
