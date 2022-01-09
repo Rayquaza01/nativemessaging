1e094f9458f5fab2d7e10959a277a5622ad01aaf
roelderickx <roel.derickx@gmail.com>
2022-01-09 10:32:57 +0100

    Raise an exception if message is empty in stead of quitting the application

--- nativemessaging/nativemessaging.py
+++ nativemessaging/nativemessaging.py
@@ -12,7 +12,7 @@ def log_browser_console(message):
 def get_message():
     raw_length = sys.stdin.buffer.read(4)
     if len(raw_length) == 0:
-        sys.exit(0)
+        raise Exception("Message is empty")
     message_length = struct.unpack("@I", raw_length)[0]
     message = sys.stdin.buffer.read(message_length).decode("utf-8")
     return json.loads(message)
